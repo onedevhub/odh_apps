449e649d17df55b2bbb272964eab17022c93ab75
onedevhub <onedevhub01@gmail.com>
2026-06-06 18:53:02 +0530

    added the bom over view app inside the app store

---
+++ odh_sale_line_bom_overview/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of OneDevHub.
+
+{
+    'name': "Sale Line BOM Overview",
+    'version': '17.0.1.0.0',
+    'category': 'Manufacturing',
+    'summary': """ Quickly view the Bill of Materials (BOM) overview directly from Sales Orders without navigating through products or manufacturing menus """,
+    'description': """ Quickly view the Bill of Materials (BOM) overview directly from Sales Orders without navigating through products or manufacturing menus """,
+    'author': 'OneDevHub',
+    'website': 'https://onedevhub.in',
+    'license': 'OPL-1',
+    'depends': ['sale_management', 'mrp'],
+    'data': [
+        'views/sale_order_views.xml',
+
+    ],
+    "price": "5.00",
+    "currency": "USD",
+    "images": ["static/description/banner_image.jpg"],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
